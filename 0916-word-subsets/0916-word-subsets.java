b45623da6f2f3cb53f7667515962b08984687cfa
Bohdan Metin <129307252+GreaTuna@users.noreply.github.com>
2025-01-10 19:31:21 +0200

    Time: 14 ms (50%), Space: 55.3 MB (15.7%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.java
+++ 0916-word-subsets/0916-word-subsets.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
         int[] arr_2 = new int[26];
-        int[] count = new int[26];
 
         for(var word : words2) {
-
+            int[] count = new int[26];
             for(char character : word.toCharArray()) {
                 count[character - 'a'] += 1;
             }
             for (int k = 0; k < 26; k++) {
                 arr_2[k] = Math.max(count[k], arr_2[k]);
             }
-            Arrays.fill(count, 0);
         }
-
-        int[] arr_1 = new int[26];
+   
         List<String> answer = new ArrayList<>();
         for(var word : words1) {
+            int[] arr_1 = new int[26];
             for (char character : word.toCharArray()) {
                 arr_1[character - 'a'] += 1;
             }
@@ -29,7 +27,6 @@ class Solution {
                 }
             }
             if (flag) answer.add(word);
-            Arrays.fill(arr_1, 0);
         }
 
         return answer;
