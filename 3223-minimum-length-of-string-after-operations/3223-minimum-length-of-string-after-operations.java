1b130983dc8f5756b9e00f110231fd3b186adcc2
Bohdan Metin <129307252+GreaTuna@users.noreply.github.com>
2025-01-13 21:38:52 +0200

    Time: 10 ms (82%), Space: 46.5 MB (34%) - LeetHub

---
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumLength(String s) {
+       int[] arr = new int[26];
+
+       for (var character : s.toCharArray()) {
+           int element = character - 97;    
+           arr[element] += 1;
+       }
+
+        int count = 0;
+        for (int element : arr) {
+            if (element > 0) {
+                if (element % 2 == 0) {
+                    count += 2;
+                } else {
+                    count++;
+                }
+            }
+       }
+
+       return count;
+    }
+}
