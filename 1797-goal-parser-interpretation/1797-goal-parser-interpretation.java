b1220f6ba74e232e558751e87616d3b8bb73b284
Bohdan Metin <129307252+GreaTuna@users.noreply.github.com>
2025-02-16 23:50:13 +0200

    Time: 0 ms (100%), Space: 41.5 MB (88.1%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -1,6 +1,23 @@
 class Solution {
     public String interpret(String command) {
-        String str = command.replace("()" , "o").replace("(al)" , "al") ; 
-        return str ;
+        StringBuilder sb = new StringBuilder(command.length());
+
+        for (int i = 0; i < command.length(); i++) {
+            var character = command.charAt(i);
+            if (character == 'G') {
+                sb.append('G');
+            }
+            else if (character == '(') {
+                if (command.charAt(i+1) == ')') {
+                    sb.append('o');
+                    i++;
+                }
+                else {
+                    sb.append("al");
+                    i += 2;
+                }
+            }
+        }
+        return sb.toString();
     }
 }
