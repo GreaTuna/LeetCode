bfec95b88ec15bd56431667d8967e217016c1f27
Bohdan Metin <129307252+GreaTuna@users.noreply.github.com>
2025-03-24 21:58:58 +0200

    Time: 0 ms (100%), Space: 41.4 MB (51.37%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -14,7 +14,7 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head.next;
 
-        while (fast != null && slow != null) {
+        while (fast != null) {
             int temp = fast.val;
             fast.val = slow.val;
             slow.val = temp;
