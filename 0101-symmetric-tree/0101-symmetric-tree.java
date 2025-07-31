d657f4e8ff6808c87e82bf39c20a976d15c878a0
Bohdan Metin <129307252+GreaTuna@users.noreply.github.com>
2025-07-31 16:32:00 +0300

    Time: 0 ms (100%), Space: 42 MB (40.77%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return aboba(root.left, root.right);
+    }
+
+    private boolean aboba(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return aboba(left.left, right.right) && aboba(left.right, right.left);
+    }
+}
