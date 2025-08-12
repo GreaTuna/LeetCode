9aae9a7029dcfc8543f57bb71ab67bcc80f36226
Bohdan Metin <129307252+GreaTuna@users.noreply.github.com>
2025-08-12 22:15:35 +0300

    Time: 2 ms (84.36%), Space: 52.1 MB (93.98%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -17,21 +17,19 @@ class Solution {
     private int goodNodes = 0;
 
     public int goodNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
         countGoodNodes(root, root.val);
         return goodNodes;
     }
 
     private void countGoodNodes(TreeNode node, int max) {
+        if (node == null) {
+            return;
+        }
         if (node.val >= max) {
             max = node.val;
             ++goodNodes;
         }
-
-        if (node.left != null) countGoodNodes(node.left, max);
-        if (node.right != null) countGoodNodes(node.right, max);
+        countGoodNodes(node.left, max);
+        countGoodNodes(node.right, max);
     }
 }
